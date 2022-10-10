d1a52a380a06e470bc086e6868f2a98826716614
Mike Ryan <mike.ryan52@gmail.com>
2022-10-10 14:09:58 -0700

    challenge

---
+++ src/Grid.tsx
@@ -0,0 +1,19 @@
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-quartz.css";
+import { useState } from "react";
+import { ColDef } from "ag-grid-community";
+import { AgGridReact } from "ag-grid-react";
+import data from "./data.json";
+
+interface RowData {
+  name: string;
+  color: string;
+}
+
+export default function Grid() {
+  return (
+    <div className="ag-theme-quartz-auto-dark">
+      <AgGridReact></AgGridReact>
+    </div>
+  );
+}
